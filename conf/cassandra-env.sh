2d9363088ffc7a628bd099b3b234cd6dc83396f1
Joseph Lynch <josephl@netflix.com>
2018-01-19 16:37:31 -0800

    Only set JMXPORT if unset
    This way if Priam sets it we should be ok.

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -172,7 +172,7 @@ fi
 # Specifies the default port over which Cassandra will be available for
 # JMX connections.
 # For security reasons, you should not expose this port to the internet.  Firewall it if needed.
-JMX_PORT="7501"
+JMX_PORT=${JMX_PORT:="7501"}
 
 JVM_OPTS_FILE=$CASSANDRA_CONF/jvm.options
 for opt in `grep "^-" $JVM_OPTS_FILE`
